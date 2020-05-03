90b70dda494dc9e2d59f53d21db7d4cb1a9323a4
jenniferho <jennifersho09@gmail.com>
2020-05-02 23:12:59 -0400

    seventh commit: added sound (turn volume up)

--- app/src/main/java/com/hojennifer/springimage/MainActivity.java
+++ app/src/main/java/com/hojennifer/springimage/MainActivity.java
@@ -2,13 +2,36 @@ package com.hojennifer.springimage;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+    MediaPlayer wind = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (wind != null) {
+            wind.pause();
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        if (wind == null) {
+            wind = MediaPlayer.create(MainActivity.this, R.raw.steadywind);
+            wind.setLooping(true);
+
+        }
+        wind.start();
     }
 }
